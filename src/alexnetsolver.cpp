42efad83a90e580695f5e02005b4ed919be853d9
Richard Zvonek <zvonek.r@seznam.cz>
2020-11-23 16:03:05 +0100

    Better results setting for AlexNet

--- src/alexnetsolver.cpp
+++ src/alexnetsolver.cpp
@@ -19,7 +19,7 @@ void AlexNetSolver::trainImpl(const TrainInputSet &trainData) {
   
   dlib::dnn_trainer<AlexNet> trainer(alexNet_, dlib::sgd());
   trainer.set_learning_rate(0.01);
-  trainer.set_min_learning_rate(0.0001);
+  trainer.set_min_learning_rate(0.001);
   trainer.set_mini_batch_size(256);
   trainer.set_iterations_without_progress_threshold(1000);
   trainer.set_max_num_epochs(300);
